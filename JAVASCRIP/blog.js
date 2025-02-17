6a5fd28e1b6802e3ca18f5d7a2fd74e5d7f9a7b2
iSourcess <123518941+iSourcess@users.noreply.github.com>
2025-02-17 16:00:11 -0600

    CSS AND JS

---
+++ JAVASCRIP/blog.js
@@ -0,0 +1,23 @@
+    const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+    
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('theme', 'light');
+        }    
+    }
+
+    // Verificar tema guardado
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        document.documentElement.setAttribute('data-theme', currentTheme);
+        if (currentTheme === 'dark') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    // Agregar el event listener
+    toggleSwitch.addEventListener('change', switchTheme, false);
